59a368175785c20dabdd78b60631967d825746b0
Riihacode <riihacode@gmail.com>
2025-06-07 11:25:33 +0700

    [UPDATE] backend_api - need testing

--- cloud_computing/backend_api/index.js
+++ cloud_computing/backend_api/index.js
@@ -42,5 +42,7 @@ app.use((req, res) => {
 });
 
 // Jalankan server
-app.listen(3000, () => console.log("Server is running on http://localhost:3000"));
+// app.listen(3000, () => console.log("Server is running on http://localhost:3000"));
 // app.listen(5000, () => console.log("Server is running on http://localhost:5000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
