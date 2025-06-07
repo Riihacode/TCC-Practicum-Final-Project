04df077bae619c52df56dee5e0c1a87a097f293c
Riihacode <riihacode@gmail.com>
2025-06-07 12:06:52 +0700

    [UPDATE] backend_api - need testing & revision port

--- cloud_computing/backend_api/index.js
+++ cloud_computing/backend_api/index.js
@@ -50,5 +50,10 @@ app.use((req, res) => {
 // app.listen(PORT, "0.0.0.0", () => {
 //     console.log(`Server is running on http://localhost:${PORT}`);
 // });
-app.listen(3000, () => console.log("Server connected successfully"));
+// app.listen(3000, () => console.log("Server connected successfully"));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
